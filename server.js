102dfc1497b121c9bc120fb4a2795a0b80a9653c
yun <yunjeoming@gmail.com>
2023-02-01 22:19:38 +0900

    Add dotenv

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
 
-app.use(express.json());
+const productRoutes = require('./routes');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true)
+mongoose.connect(process.env.MONGO_URL, {}).then(() => console.log('MongoDb connected')).catch(err => console.log(err))
+
+app.use('/api/products', productRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
